9ced88544aecd9ae3b1ac66634f1c6e31419f5f7
윤국현 <djaudwls7873@gmail.com>
2023-08-28 14:14:13 +0900

    호출 시그니처, 인덱스 시그니처

---
+++ 05.call-signature,index-signature/src/main.ts
@@ -0,0 +1,39 @@
+// interface getLikeNumber {
+//   (like: number): number;
+// }
+
+// interface Post {
+//   id: number;
+//   title: string;
+//   getLikeNumber: getLikeNumber;
+// }
+
+// const post1: Post = {
+//   id: 1,
+//   title: "post 1",
+//   getLikeNumber(like: number) {
+//     return like;
+//   },
+// };
+
+// post1.getLikeNumber(1);
+
+// index signature
+interface Post {
+  [key: string]: unknown;
+  id: number;
+  title: string;
+}
+const post1: Post = {
+  id: 1,
+  title: "post 1",
+};
+
+post1["description"] = "description 1";
+post1["pages"] = 300;
+
+interface Names {
+  [item: number]: string;
+}
+const userNames: Names = ["John", "Kim", "Joe"];
+userNames[0] === "John";
